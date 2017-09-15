f93e149dde81a956d68f37537395b41f01ae483a
Vu Quoc Thanh <vqthanh1412489@gmail.com>
2017-09-15 09:57:04 +0700

    Add f.js

---
+++ Buoi5 Array/f.js
@@ -0,0 +1,4 @@
+const a = ['NodeJS', 'ReactJS', 'Angular', 'Native'];
+
+a.splice(1, 2);
+console.log(a); // Nhớ là a.splice(1 , 2)  : Chứa những phần tử được xóa .Nền khi truyển thẳng nó và console.log(..) thì sẽ ra những phần từ được xóa chứ không ra những phần tử còn lại
