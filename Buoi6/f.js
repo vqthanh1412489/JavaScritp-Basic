49d6a26df50566bab8dc86d7521361974f312388
Vu Quoc Thanh <vqthanh1412489@gmail.com>
2017-09-17 10:38:56 +0700

    Them file f.js

---
+++ Buoi6/f.js
@@ -0,0 +1,10 @@
+// Sự khác nhau giữa map() và forEach()
+// forEach () :Chỉ đươn giản là duyệt qua 1 lẩn rồi áp dụng
+// map() là tạo ra 1 mảng mới
+let arr = [1, 5, 2, 10, 13];
+
+// console.log(arr.map(e => e));
+// console.log(arr);
+
+arr.forEach(e => e);
+console.log(arr);
